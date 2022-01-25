b919eef86d34013c26339217528b9329f1adee50
Mountafougoumba <Mountafougoumba@DESKTOP-N4EE533.lan>
2022-01-24 19:31:56 -0800

    First Commit

---
+++ telemedecineBE/src/main/java/com/telemedecineBE/TelemedecineBeApplication.java
@@ -0,0 +1,13 @@
+package com.telemedecineBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedecineBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemedecineBeApplication.class, args);
+	}
+
+}
---
+++ telemedecineBE/src/test/java/com/telemedecineBE/TelemedecineBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemedecineBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedecineBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
