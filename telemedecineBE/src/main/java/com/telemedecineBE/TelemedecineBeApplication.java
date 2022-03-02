51cb59afd5b72b9ddb3346457aca7613fce5589f
fswetnam <95920473+fswetnam@users.noreply.github.com>
2022-03-01 19:22:59 -0500

    Had to add this to get Angular and Spring Boot working together

--- telemedecineBE/src/main/java/com/telemedecineBE/TelemedecineBeApplication.java
+++ telemedecineBE/src/main/java/com/telemedecineBE/TelemedecineBeApplication.java
@@ -3,6 +3,12 @@ package com.telemedecineBE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TelemedecineBeApplication {
@@ -11,4 +17,20 @@ public class TelemedecineBeApplication {
 		SpringApplication.run(TelemedecineBeApplication.class, args);
 	}
 
+	//Had to add to get Angular, Spring Boot, and Chrome working
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
