f3be69807515250b7a93e6312ece039b707c731b
beinglew <35906276+beinglew@users.noreply.github.com>
2022-04-19 18:03:28 -0400

    Update UserController.java

--- telemedecineBE/src/main/java/com/telemedecineBE/web/UserController.java
+++ telemedecineBE/src/main/java/com/telemedecineBE/web/UserController.java
@@ -1,21 +1,36 @@
 package com.telemedecineBE.web;
 
-import  com.telemedecineBE.TelemedecineBeApplication;
+
+import com.telemedecineBE.Security.JwtUtil;
+import com.telemedecineBE.dao.RoleRepository;
 import com.telemedecineBE.entities.User;
+
 import com.telemedecineBE.dao.PatientRepository;
+
 import com.telemedecineBE.dao.UserDao;
 import com.telemedecineBE.enumeration.UserType;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/return")
@@ -24,14 +39,22 @@ public class UserController {
     @Autowired
     private UserDao userDao;
 
-    /*
     @Autowired
     private PasswordEncoder passwordEncoder;
-     */
+
+    @Autowired
+    RoleRepository roleRepository;
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/login")
     User loginUser(@RequestBody Map<String, String> userMap){
-        String email = userMap.get("email");
-        String password = userMap.get("password");
+        String email = userMap.get("userName");
+        String password = userMap.get("userpassword");
         Boolean validUser = userDao.existsByEmail(email);
 
         if(!validUser) {
@@ -52,16 +75,99 @@ public class UserController {
 
         return user;
     }
-    
+
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> generateAuthenticationToken(@RequestBody User user)throws Exception {
+        System.out.println("Authenticating");
+        System.out.println(user.getUsername());
+        System.out.println(user.getUserpassword());
+
+        Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(), user.getPassword()
+                ));
+
+
+        user = (User) authenticate.getPrincipal();
+        user.setUserpassword(null);
+        System.out.println(user.getPassword());
+        System.out.println();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("JWT-TOKEN", jwtUtil.generateToken(user));
+        responseHeaders.add("Access-Control-Exposed", "Authorization");
+
+
+           /* ResponseEntity.ok().header(
+                            HttpHeaders.AUTHORIZATION,
+                            jwtUtil.generateToken(user)//, jwtUtil.generateToken(user)
+                    )
+                    .body(user);
+
+            */
+        return ResponseEntity.ok(jwtUtil.generateToken(user));
+    }
+
+
+
+
+
+
+    @GetMapping("/validate")
+    public ResponseEntity<?> validateToken(@RequestParam String token, @AuthenticationPrincipal User user){
+        try {
+            Boolean isTokenValid = jwtUtil.validateToken(token, user);
+            return ResponseEntity.ok(isTokenValid);
+            }
+        catch (ExpiredJwtException e){
+            return ResponseEntity.ok(false);
+        }
+
+
+    }
+
     @GetMapping("/users")
     List<User> getAllUsers(){
         System.out.println("getAllUsers");
         return userDao.findAll();
     }
+    @PostMapping("/user")
+    User newUser(@RequestBody User user){
+        Boolean exists = userDao.existsByEmail(user.getEmail());
+        //Boolean exists2 = patientRepository.existsByPhone(patient.getPhone());
+        if(exists){
+            throw new IllegalStateException("Patient with email " + user.getEmail() + " already exists.");
+        } //else if(exists2){
+        //throw new IllegalStateException("Patient with phone " + patient.getPhone() + " already exists.");
+        //}
+        String test;
+        user.setUserType(UserType.PATIENT);
+
+
+        userDao.save(user);
+        System.out.println("newPatient");
+
+        return user;
+    }
+    // Create User
+    @GetMapping("/register")
+    @ResponseBody
+    public String create(String userName, String userpassword) {
+        User user = null;
+        try {
+            user = new User(userName, passwordEncoder.encode(userpassword));
+            userDao.save(user);
+        }
+        catch (Exception ex){
+            return "Error creating user:" + userName;
+        }
+        return "User registered succesfully! (username = " + user.getUsername() + ")";
+    }
+
 
     
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
 
     @Autowired
     private PatientRepository patientRepository;
@@ -83,8 +189,8 @@ public class UserController {
             currentUser.setFname(user.getFname());
         }
 
-        if(user.getUserName() != null && user.getUserName() .length() > 0 && user.getUserName()  != currentUser.getUserName() && !userDao.existsByUserName(user.getUserName())){
-            currentUser.setUserName(user.getUserName() );
+        if(user.getUsername() != null && user.getUsername() .length() > 0 && user.getUsername()  != currentUser.getUsername() && !userDao.existsByUserName(user.getUsername())){
+            currentUser.setUserName(user.getUsername() );
         }
 
         if(user.getUserpassword() != null && user.getUserpassword().length() > 0 && user.getUserpassword() != currentUser.getUserpassword()){
@@ -116,6 +222,7 @@ public class UserController {
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
         if (auth != null){
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
@@ -129,7 +236,7 @@ public class UserController {
                         @RequestParam(required = false)String fName,
                         @RequestParam(required = false)String username,
                         @RequestParam(required = false)String password,
-                        @RequestParam(required = false)UserType userType,
+                        @RequestParam(required = false) UserType userType,
                         @RequestParam(required = false)String email,
                         @RequestParam(required = false)String phone,
                         @RequestParam(required = false)Integer state){
@@ -144,7 +251,7 @@ public class UserController {
             user.setFname(fName);
         }
 
-        if(username != null && username.length() > 0 && username != user.getUserName() && !userDao.existsByUserName(username)){
+        if(username != null && username.length() > 0 && username != user.getUsername() && !userDao.existsByUserName(username)){
             user.setUserName(username);
         }
 
@@ -171,13 +278,28 @@ public class UserController {
         return user;
     }
 
-    @PutMapping("/user/phone={phone}")
+    /*
+    @PostMapping("/user")
+    User newUser(@RequestBody User user) {
+        Boolean exists = userDao.existsByEmail(user.getEmail());
+        //Boolean exists2 = patientRepository.existsByPhone(patient.getPhone());
+        if (exists) {
+            throw new IllegalStateException("Patient with email " + user.getEmail() + " already exists.");
+        } //else if(exists2){
+        //throw new IllegalStateException("Patient with phone " + patient.getPhone() + " already exists.");
+        //}
+        String test;
+        user.setUserType("PATIENT");
+    }
+*/
+
+        @PutMapping("/user/phone={phone}")
     User updateUserByPhone(@PathVariable(value = "phone")String currentPhone,
                            @RequestParam(required = false)String lName,
                            @RequestParam(required = false)String fName,
                            @RequestParam(required = false)String username,
                            @RequestParam(required = false)String password,
-                           @RequestParam(required = false)UserType userType,
+                           @RequestParam(required = false) UserType userType,
                            @RequestParam(required = false)String email,
                            @RequestParam(required = false)String phone,
                            @RequestParam(required = false)Integer state){
@@ -195,7 +317,7 @@ public class UserController {
             user.setFname(fName);
         }
 
-        if(username != null && username.length() > 0 && username != user.getUserName() && !userDao.existsByUserName(username)){
+        if(username != null && username.length() > 0 && username != user.getUsername() && !userDao.existsByUserName(username)){
             user.setUserName(username);
         }
 
@@ -236,4 +358,42 @@ public class UserController {
         }
         return "Login Page";
     }
+
+     /*Create User. Edit to add encoded passwords to database
+
+        System.out.println("newPatient");
+
+        return user;
+    }
+    // Create User
+    @GetMapping("/register")
+    @ResponseBody
+    public String create(String userName, String userpassword){
+        User user = null;
+        try {
+            user = new User(userName, passwordEncoder.encode(userpassword));
+            userDao.save(user);
+        }
+        catch (Exception ex){
+            return "Error creating user:" + userName;
+        }
+        return "User registered succesfully! (username = " + user.getUserName() + ")";
+    } */
+
+   /*@RequestMapping("/update")
+    @ResponseBody
+    public String updateUser(Integer id,String username, String name) {
+        User user = (User) userDao.findByUserName(username);
+        userDao.save(user);
+        // user.setUserName(username);
+        user.setFname(name);
+        return "User succesfully updated: ";
+    }
+
+    */
+
+
+
+
+
 }
