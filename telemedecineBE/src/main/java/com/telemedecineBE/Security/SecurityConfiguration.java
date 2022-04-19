6a8f9db24b87b446bbaf3663437cdeca3fb26fdc
beinglew <35906276+beinglew@users.noreply.github.com>
2022-04-19 17:56:59 -0400

    Update SecurityConfiguration.java

--- telemedecineBE/src/main/java/com/telemedecineBE/Security/SecurityConfiguration.java
+++ telemedecineBE/src/main/java/com/telemedecineBE/Security/SecurityConfiguration.java
@@ -8,18 +8,24 @@ import com.telemedecineBE.dao.AdminRepository;
 import com.telemedecineBE.dao.UserDao;
 import com.telemedecineBE.dao.UserRepository;
 import com.telemedecineBE.entities.Admin;
-import com.telemedecineBE.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 // Basic Login neeed to connect to databse
@@ -32,28 +38,49 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     UserDao userDao;
     @Autowired
     AdminRepository adminRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserDetailsServiceImplenets userDetailsServiceImplenets;
+    @Autowired
+    JwtFilter jwtFilter;
+
+    @Override @Bean
+    public AuthenticationManager authenticationManagerBean() throws  Exception{
+        return super.authenticationManagerBean();
+    }
+
+    public void SecurityConfig(UserRepository userRepository){
+        this.userRepository = userRepository;
+
+    }
+
+
     @Override
     public void configure(HttpSecurity http) throws Exception{
+        http = http.csrf().disable().cors().disable();
 
+
+        http = http.sessionManagement()
+                       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                               .and();
+
+        http = http.exceptionHandling()
+                        .authenticationEntryPoint((request, responce, ex) ->{
+                                responce.sendError(HttpServletResponse.SC_UNAUTHORIZED , ex.getMessage());
+                                }).and();
+
+
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         http
-
-                .csrf().disable()
-                .cors().disable()
                 .authorizeRequests()
-                .antMatchers("/register",
-                        "/login", "/logout",
-                        "/patient", "/patient/**", "/patients",
-                        "/address", "/addresses", "/address/**",
-                        "/insurance", "/insurances", "/insurance/**",
-                        "/user", "/users", "/user/**",
-                        "/doctors", "/doctor", "/doctor/**",
-                        "/admin", "/admins", "/admin/**",
-                        "/request", "/requests", "/request/**",
-                        "/message", "/messages", "/message/**","/messages/**",
-                        "/prescriptions", "/prescription", "/prescription/**",
-                        "/appointment", "/appointments", "/appointment/**", "/video-chat-room", "/video-chat-room/**",
-                            "/report", "/reports", "/report/**").permitAll()
+                .antMatchers("/validate","/authenticate").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .antMatchers(HttpMethod.POST,
+                        "/login", "/logout").permitAll()
+                .antMatchers(HttpMethod.PUT, "/register").permitAll()
                 .anyRequest().authenticated()
                // .and()
                 //.formLogin()
@@ -61,18 +88,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .logout()
+                .addLogoutHandler((request, response, authentication) -> {
+
+                })
                 .logoutSuccessUrl("/login")
                 .permitAll();
+
+        http.addFilterBefore(
+                jwtFilter,UsernamePasswordAuthenticationFilter.class
+        );
     }
 
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        auth.userDetailsService(userDetailsServiceImplenets).passwordEncoder(passwordEncoder());
+
+
+
+
+
+
+
         List<Admin> admins = adminRepository.findAll();
         for (Admin user: admins) {
             auth.inMemoryAuthentication()
-                    .withUser(user.getUserName())
+                    .withUser(user.getUsername())
                     .password(user.getUserpassword())
                     .roles("ADMIN");
         }
