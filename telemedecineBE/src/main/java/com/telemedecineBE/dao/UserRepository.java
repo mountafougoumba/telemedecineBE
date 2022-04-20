e2f3dcfd97260ec8e7b38db7ede7f5bfa84d7905
awells58 <90581577+awells58@users.noreply.github.com>
2022-04-19 22:31:59 -0400

    Fix missing optional import

--- telemedecineBE/src/main/java/com/telemedecineBE/dao/UserRepository.java
+++ telemedecineBE/src/main/java/com/telemedecineBE/dao/UserRepository.java
@@ -2,9 +2,8 @@ package com.telemedecineBE.dao;
 
 import com.telemedecineBE.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findUserByUserNameEquals(String name);
