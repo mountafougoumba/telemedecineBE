be3365d065b3262e479cc46f3657c9dbf3d0bb2d
awells58 <90581577+awells58@users.noreply.github.com>
2022-05-02 20:51:34 -0400

    Update SearchController.java

--- telemedecineBE/src/main/java/com/telemedecineBE/web/SearchController.java
+++ telemedecineBE/src/main/java/com/telemedecineBE/web/SearchController.java
@@ -26,14 +26,9 @@ public class SearchController {
        try {
            List<User> users = this.userRepository.searchUsers(term);
            System.out.println(users);
-
            ObjectMapper mapper = new ObjectMapper();
-
            HashMap<String, String> map = new HashMap<>();
            map.put("users", mapper.writeValueAsString(users));
-           map.put("foo", "bar");
-           map.put("aa", "bb");
-
            return map;
        } catch (Exception e) {
             return new HashMap<>();
